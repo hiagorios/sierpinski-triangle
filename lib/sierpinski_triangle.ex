3bd65b18b9b5b9a0392a6d7e9e8b343889814fe9
Hiago Rios <hriosdev@gmail.com>
2021-07-16 12:36:04 -0300

    feat: trying to tint matrix

--- lib/sierpinski_triangle.ex
+++ lib/sierpinski_triangle.ex
@@ -58,57 +58,84 @@ defmodule SierpinskiTriangle do
   @impl true
   def start(_type, _args) do
     # Main call
-    # triangles =
-    SierpinskiTriangle.generate(1, :test)
-  end
-
-  def split_triangle(0, triangle, image) do
-    half_trg_wd = (triangle.right_col - triangle.left_col) / 2
-
-    new_image =
-      for y <- triangle.top..(triangle.top + triangle.height) do
-        for x <- triangle.left_col..triangle.right_col do
-          if half_trg_wd - y <= x and x <= half_trg_wd + y do
-            List.replace_at([], y * half_trg_wd * 2 + x, @filled_char)
-          end
-        end
-      end
+    n = 1
+    empty_image = List.duplicate(@empty_char, @columns * @rows)
+    image = SierpinskiTriangle.split_triangle(n, %Triangle{}, empty_image)
+    SierpinskiTriangle.print(image)
   end
 
   def split_triangle(n, triangle, image) do
-    width = triangle.right_col - triangle.left_col
+    case 0 do
+      ^n ->
+        # base case
+        half_trg_wd = (triangle.right_col - triangle.left_col) / 2
+        y = triangle.top_row
+        y_final = triangle.top_row + triangle.height
+        tint_image(image, y, y_final, half_trg_wd)
 
-    top = %Triangle{
-      top_row: triangle.top,
-      left_col: 1 / 4 * width,
-      right_col: 3 / 4 * width,
-      height: triangle.height / 2
-    }
+      _ ->
+        # general case
+        width = triangle.right_col - triangle.left_col
 
-    left = %Triangle{
-      top_row: top.left_col,
-      left_col: triangle.left_col,
-      right_col: width / 2,
-      height: triangle.height / 2
-    }
+        top = %Triangle{
+          top_row: triangle.top_row,
+          left_col: 1 / 4 * width,
+          right_col: 3 / 4 * width,
+          height: triangle.height / 2
+        }
 
-    right = %Triangle{
-      top_row: top.right_col,
-      left_col: left.right_col,
-      right_col: triangle.right_col,
-      height: triangle.height / 2
-    }
+        left = %Triangle{
+          top_row: top.left_col,
+          left_col: triangle.left_col,
+          right_col: width / 2,
+          height: triangle.height / 2
+        }
 
-    image = split_triangle(n - 1, top, image)
-    image = split_triangle(n - 1, left, image)
-    split_triangle(n - 1, right, image)
+        right = %Triangle{
+          top_row: top.right_col,
+          left_col: left.right_col,
+          right_col: triangle.right_col,
+          height: triangle.height / 2
+        }
+
+        image_modified = split_triangle(n - 1, top, image)
+        image_modified_again = split_triangle(n - 1, left, image_modified)
+        split_triangle(n - 1, right, image_modified_again)
+    end
   end
 
-  def generate(0, state) do
-    IO.puts("end")
+  def tint_image(image, y, y_final, half_trg_wd) do
+    x = half_trg_wd - y
+    x_final = half_trg_wd + y
+    offset = y * half_trg_wd * 2
+    image_modified = tint_line(image, offset, x, x_final)
+
+    case y do
+      ^y_final ->
+        image_modified
+
+      _ ->
+        tint_image(image_modified, y + 1, y_final, half_trg_wd)
+    end
   end
 
-  def generate(n, state) do
-    [head | tail] = generate(n - 1, state)
+  def tint_line(image, offset, x, x_final) do
+    image_modified = List.replace_at(image, offset + x, @filled_char)
+
+    case x do
+      x_final ->
+        image_modified
+
+      _ ->
+        tint_line(image_modified, offset, x + 1, x_final)
+    end
+  end
+
+  def print(image) do
+    for y <- 0..@rows do
+      for x <- 0..@columns do
+        IO.write(Enum.at(image, y * @columns + x))
+      end
+    end
   end
 end
